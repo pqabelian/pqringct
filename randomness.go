ed0e77f691f3f21bc54dbe7ebeb9ef8e6b2412a0
depression <793377875@qq.com>
2022-04-11 21:24:18 +0800

    Rename randomnessv2 to randomness

--- randomness.go
+++ randomness.go
@@ -1,2 +1,300 @@
 package pqringct
 
+import (
+	"crypto/rand"
+	"errors"
+	"golang.org/x/crypto/sha3"
+)
+
+var ErrLength = errors.New("invalid length")
+
+const RandSeedBytesLen = 64 // 512-bits
+
+// extendable output function is instanced as sha3.Shake128()
+// to get expected length number but the input of sha3.Shake128()
+// is output of sha3.Sha512() function.
+
+func fillWithBound(buf []byte, length int, bitNum int, bound int64) []int64 {
+	res := make([]int64, 0, length)
+	// 首先计算bitNum和8的最小公倍数，每次可以拿出needPer个byte生成
+	g := gcd(bitNum, 8)
+	needPer, gotPer := bitNum/g, 8/g
+	pos := 0
+	// 每次取needPer个byte
+	for pos+needPer-1 < len(buf) {
+		for i := 0; i < gotPer; i++ {
+			t := int64(0)
+			// [0,needPer*8] 中取出 [i*bitNum,(i+1)*bitNum]
+			for j := i * bitNum; j < (i+1)*bitNum; j++ {
+				t |= int64((buf[pos+j/8]&(1<<(j%8)))>>(j%8)) << (j - i*bitNum)
+			}
+			if t <= bound {
+				res = append(res, t)
+				if len(res) == length {
+					return res
+				}
+			}
+		}
+		pos += needPer
+	}
+	return res
+}
+func gcd(a int, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+// RandomBytes returns a byte array with given length from crypto/rand.Reader
+func RandomBytes(length int) []byte {
+	res := make([]byte, 0, length)
+	var tmp []byte
+	for length > 0 {
+		tmp = make([]byte, length)
+		// n == len(b) if and only if err == nil.
+		n, err := rand.Read(tmp)
+		if err != nil {
+			continue
+		}
+		res = append(res, tmp[:n]...)
+		length -= n
+	}
+	return res
+}
+
+// 523987 = 0111_1111_1110_1101_0011
+// randomPolyAForResponseZetaA() returns a PolyA, where each coefficient lies in [-(eta_a - beta_a), (eta_a - beta_a)],
+// where eta_a = 2^{19}-1 and beta=300
+func (pp *PublicParameter) randomPolyAForResponseZetaA() (*PolyA, error) {
+	bound := int64(523987) // 1 << 19 - 1 - 300
+	length := pp.paramDA   // 128
+	coeffs := make([]int64, length)
+
+	seed := RandomBytes(RandSeedBytesLen)
+	xof := sha3.NewShake128()
+	xof.Reset()
+	_, err := xof.Write(seed)
+	if err != nil {
+		return nil, err
+	}
+	var buf []byte
+	// random the number in range [0,2*bound], and then reduce to [-bound, bound]
+	// 2*bound=0b1111_1111_1101_1010_0110, means that an element needs 20 bits
+	// expected (20 * length * (1<<19) / bound + 7 ) / 8 bytes
+	buf = make([]byte, (20*int64(length)*(1<<19)/bound+7)/8)
+	_, err = xof.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	cur := 0
+	res := fillWithBound(buf, length, 20, 2*bound)
+	for i := 0; i < len(res); i++ {
+		coeffs[cur+i] = res[i] - bound
+	}
+	cur += len(res)
+	for cur < length {
+		// uniform reject sample from the buf
+		buf = make([]byte, 5) // gcd(20,8)=4*5*2=8*5
+		_, err = xof.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		res = fillWithBound(buf, length-cur, 20, 2*bound)
+		for i := 0; i < len(res); i++ {
+			coeffs[cur+i] = res[i] - bound
+		}
+		cur += len(res)
+	}
+	return &PolyA{coeffs}, nil
+}
+
+// 16777087 = 1111_1111_1111_1111_0111_1111
+// randomPolyCForResponseZetaC() returns a PolyC, where each coefficient lies in [-(eta_c - beta_c), (eta_c - beta_c)],
+// where eta_c = 2^{24}-1 and beta_c=128
+func (pp *PublicParameter) randomPolyCForResponseZetaC() (*PolyC, error) {
+	bound := int64(16777087)
+	length := pp.paramDC // todo: fix the length to be pp.paramDC, will there is optimization?
+	coeffs := make([]int64, length)
+
+	seed := RandomBytes(RandSeedBytesLen)
+	xof := sha3.NewShake128()
+	xof.Reset()
+	_, err := xof.Write(seed)
+	if err != nil {
+		return nil, err
+	}
+	var buf []byte
+	// random the number in range [0,2*bound], and then reduce to [-bound, bound]
+	// 2*bound=0b00001_1111_1111_1111_1110_1111_1110, means that an element needs 25 bits
+	// expected (25 * length * (1<<24) / bound + 7 ) / 8 bytes
+	buf = make([]byte, (25*int64(length)*(1<<24)/bound+7)/8)
+	_, err = xof.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	cur := 0
+	// uniform reject sample from the buf
+	res := fillWithBound(buf, length, 25, 2*bound)
+	for i := 0; i < len(res); i++ {
+		coeffs[cur+i] = res[i] - bound
+	}
+	cur += len(res)
+	for cur < length {
+		// uniform reject sample from the buf
+		buf = make([]byte, 25) // gcd(25,8)=8*25
+		_, err = xof.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		res = fillWithBound(buf, length-cur, 25, 2*bound)
+		for i := 0; i < len(res); i++ {
+			coeffs[cur+i] = res[i] - bound
+		}
+		cur += len(res)
+	}
+
+	return &PolyC{coeffs}, nil
+}
+
+// 2^24-1= 1111_1111_1111_1111_1111_1111
+//	randomPolyCinEtaC() outputs a PolyC, where each coefficient lies in [-eta_c, eta_c].
+//	eta_c = 2^{24}-1, so that each coefficient needs 3 bytes (for absolute) and 1 bit (for signal)
+func (pp *PublicParameter) randomPolyCinEtaC() (*PolyC, error) {
+	bound := int64(1<<24 - 1)
+	length := pp.paramDC // todo: fix the length to be pp.paramDC, will there is optimization?
+	coeffs := make([]int64, length)
+
+	seed := RandomBytes(RandSeedBytesLen)
+	xof := sha3.NewShake128()
+	xof.Reset()
+	_, err := xof.Write(seed)
+	if err != nil {
+		return nil, err
+	}
+	var buf []byte
+	// random the number in range [0,2*bound], and then reduce to [-bound, bound]
+	// 2*bound=0b00001_1111_1111_1111_1110_1111_1110, means that an element needs 25 bits
+	// expected (25 * length * (1<<24) / bound + 7 ) / 8 bytes
+	buf = make([]byte, (25*int64(length)*(1<<24)/bound+7)/8)
+	_, err = xof.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	cur := 0
+	// uniform reject sample from the buf
+	res := fillWithBound(buf, length, 25, 2*bound)
+	for i := 0; i < len(res); i++ {
+		coeffs[cur+i] = res[i] - bound
+	}
+	cur += len(res)
+	for cur < length {
+		// uniform reject sample from the buf
+		buf = make([]byte, 25) // gcd(25,8)=8*25
+		_, err = xof.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		res = fillWithBound(buf, length-cur, 25, 2*bound)
+		for i := 0; i < len(res); i++ {
+			coeffs[cur+i] = res[i] - bound
+		}
+		cur += len(res)
+	}
+
+	return &PolyC{coeffs}, nil
+}
+
+//	randomPolyAinEtaA() outputs a PolyA, where each coefficient lies in [-eta_a, eta_a].
+//	eta_a = 2^{19}-1, so that each coefficient needs 20 bits to sample, say 19 bits (for absolute) and 1 bit (for signal).
+//	That is, we can use 5 byets to sample 2 coefficients.
+func (pp *PublicParameter) randomPolyAinEtaA() (*PolyA, error) {
+	bound := int64(1<<19 - 1)
+	length := pp.paramDA
+	coeffs := make([]int64, length)
+
+	seed := RandomBytes(RandSeedBytesLen)
+	xof := sha3.NewShake128()
+	xof.Reset()
+	_, err := xof.Write(seed)
+	if err != nil {
+		return nil, err
+	}
+	var buf []byte
+	// random the number in range [0,2*bound], and then reduce to [-bound, bound]
+	// 2*bound=0b00001_1111_1111_1111_1110_1111_1110, means that an element needs 25 bits
+	// expected (25 * length * (1<<24) / bound + 7 ) / 8 bytes
+	buf = make([]byte, (20*int64(length)*(1<<19)/bound+7)/8)
+	_, err = xof.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	cur := 0
+	// uniform reject sample from the buf
+	res := fillWithBound(buf, length, 20, 2*bound)
+	for i := 0; i < len(res); i++ {
+		coeffs[cur+i] = res[i] - bound
+	}
+	cur += len(res)
+	for cur < length {
+		// uniform reject sample from the buf
+		buf = make([]byte, 5) // gcd(20,8)=4*5*2=8*5
+		_, err = xof.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		res = fillWithBound(buf, length-cur, 20, 2*bound)
+		for i := 0; i < len(res); i++ {
+			coeffs[cur+i] = res[i] - bound
+		}
+		cur += len(res)
+	}
+
+	return &PolyA{coeffs}, nil
+}
+
+// [-5,5]
+func (pp *PublicParameter) randomPolyAinGammaA5(seed []byte) (*PolyA, error) {
+	bound := int64(5)
+	length := pp.paramDA
+	coeffs := make([]int64, length)
+
+	if seed == nil {
+		seed = RandomBytes(RandSeedBytesLen)
+	}
+	xof := sha3.NewShake128()
+	xof.Reset()
+	_, err := xof.Write(seed)
+	if err != nil {
+		return nil, err
+	}
+	var buf []byte
+	// random the number in range [0,2*bound], and then reduce to [-bound, bound]
+	// 2*bound=0b1111_1111_1101_1010_0110, means that an element needs 20 bits
+	// expected (20 * length * (1<<19) / bound + 7 ) / 8 bytes
+	buf = make([]byte, (4*int64(length)*(1<<3)/bound+7)/8)
+	_, err = xof.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	cur := 0
+	res := fillWithBound(buf, length, 4, 2*bound)
+	for i := 0; i < len(res); i++ {
+		coeffs[cur+i] = res[i] - bound
+	}
+	cur += len(res)
+	for cur < length {
+		// uniform reject sample from the buf
+		buf = make([]byte, 3) // gcd(20,8)=4*5*2=8*5
+		_, err = xof.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		res = fillWithBound(buf, length-cur, 4, 2*bound)
+		for i := 0; i < len(res); i++ {
+			coeffs[cur+i] = res[i] - bound
+		}
+		cur += len(res)
+	}
+	return &PolyA{coeffs}, nil
+}
--- randomness_test.go
+++ randomness_test.go
@@ -1,97 +1,478 @@
 package pqringct
 
-//func Test_randomnessFromProbabilityDistributions(t *testing.T) {
-//	type args struct {
-//		seed   []byte
-//		length int
-//	}
-//	tests := []struct {
-//		name    string
-//		args    args
-//		want    []int32
-//		wantErr bool
-//	}{
-//		{
-//			name: "one",
-//			args: args{
-//				seed:   []byte{0b0000_0001, 0b0010_0011, 0b0100_0101, 0b0110_0111, 0b1000_1001, 0b1010_1011, 0b1100_1101, 0b1110_1111},
-//				length: 16,
-//			},
-//			want:    []int32{-1, 0, 1, -1, 0, 1, -1, 0, 0, 1, -1, 0, 1, -1, 0, 1},
-//			wantErr: false,
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			got, _, err := randomnessFromProbabilityDistributions(tt.args.seed, tt.args.length)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("randomnessFromProbabilityDistributions() error = %value, wantErr %value", err, tt.wantErr)
-//				return
-//			}
-//			if !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("randomnessFromProbabilityDistributions() got = %value, want %value", got, tt.want)
-//			}
-//		})
-//	}
-//}
+import (
+	"fmt"
+	"log"
+	"testing"
+)
 
-//func Test_randomnessFromChallengeSpace(t *testing.T) {
-//	type args struct {
-//		seed   []byte
-//		length int
-//	}
-//	tests := []struct {
-//		name    string
-//		args    args
-//		want    []int32
-//		wantErr bool
-//	}{
-//		{
-//			name: "test",
-//			args: args{
-//				seed:   []byte{0b00_01_10_11},
-//				length: 4,
-//			},
-//			want:    []int32{0, -1, 1, 0},
-//			wantErr: false,
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			got, err := randomnessFromChallengeSpace(tt.args.seed, tt.args.length)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("randomnessFromChallengeSpace() error = %value, wantErr %value", err, tt.wantErr)
-//				return
-//			}
-//			if !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("randomnessFromChallengeSpace() got = %value, want %value", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func TestNaive_randomBytes(t *testing.T) {
+	tests := []struct {
+		name      string
+		times     int
+		precision float64
+		baseline  float64
+		manual    bool
+	}{
+		{
+			name:      "10000Time",
+			times:     10_000,
+			precision: 1e-3,
+			baseline:  0.5,
+			manual:    false,
+		},
+		{
+			name:      "50000Time",
+			times:     50_000,
+			precision: 1e-3,
+			baseline:  0.5,
+			manual:    false,
+		},
+		{
+			name:      "100000Time",
+			times:     100_000,
+			precision: 1e-4,
+			baseline:  0.5,
+			manual:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			count0 := 0
+			count1 := 0
+			var rst []byte
+			for count := 0; count < tt.times; count++ {
+				rst = RandomBytes(RandSeedBytesLen)
+				for i := 0; i < RandSeedBytesLen; i++ {
+					tmp := rst[i]
+					for j := 0; j < 8; j++ {
+						if (tmp>>j)&1 == 1 {
+							count1++
+						} else {
+							count0++
+						}
+					}
+				}
+			}
+			total := float64(count0 + count1)
+			fmt.Println("number of 0:", count0, "persent:", float64(count0)/total)
+			fmt.Println("number of 1:", count0, "persent:", float64(count1)/total)
+			if float64(count0)/total-tt.baseline > tt.precision {
+				t.Errorf("uneven")
+			}
+			if float64(count0)/total-tt.baseline > tt.precision {
+				t.Errorf("uneven")
+			}
+			if tt.manual {
+				for i := 0; i < len(rst); i++ {
+					fmt.Println("i=", i, "byte:", rst[i])
+				}
+			}
+		})
+	}
+}
 
-//func Test_randomBytes(t *testing.T) {
-//	type args struct {
-//		length int
-//	}
-//	tests := []struct {
-//		name string
-//		args args
-//		want []byte
-//	}{
-//		{
-//			name: "1",
-//			args: args{length: 32},
-//			want: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			got := RandomBytes(tt.args.length)
-//			fmt.Println(got)
-//			if reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("RandomBytes() = %value, want %value", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func TestNaive_randomnessPolyAForResponseZetaA(t *testing.T) {
+	pp := DefaultPP
+	tests := []struct {
+		name      string
+		times     int
+		slotNum   int
+		precision float64
+		baseline  float64
+		manual    bool
+	}{
+		{
+			name:      "10000Time",
+			times:     10_000,
+			slotNum:   10,
+			precision: 1e-1,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "50000Time",
+			times:     50_000,
+			slotNum:   10,
+			precision: 1e-2,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "100000Time",
+			times:     100_000,
+			slotNum:   10,
+			precision: 1e-3,
+			baseline:  0.1,
+			manual:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			count := make([]int, tt.slotNum)
+			step := (pp.paramEtaA - int64(pp.paramBetaA) + 4) / 5
+			start := -(pp.paramEtaA - int64(pp.paramBetaA))
+			end := pp.paramEtaA - int64(pp.paramBetaA)
+			var polyA *PolyA
+			var err error
+			for cnt := 0; cnt < tt.times; cnt++ {
+				polyA, err = pp.randomPolyAForResponseZetaA()
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				for i := 0; i < pp.paramDA; i++ {
+					switch {
+					case polyA.coeffs[i] < start:
+						t.Fatal("ERROR: Sample in left out")
+					case polyA.coeffs[i] > end:
+						t.Fatal("ERROR: Sample in right out")
+					default:
+						slot := (polyA.coeffs[i] - start) / step
+						count[slot] = count[slot] + 1
+					}
+				}
+			}
+			total := 0
+			for i := 0; i < tt.slotNum; i++ {
+				total += count[i]
+			}
+			for i := 0; i < tt.slotNum; i++ {
+				ratio := float64(count[i]) / float64(total)
+				if ratio-tt.baseline > tt.precision {
+					t.Errorf("slot %d, number %v, percent:%v", i, count[i], ratio)
+				}
+			}
+
+			if tt.manual {
+				for i := 0; i < pp.paramDA; i++ {
+					fmt.Println(polyA.coeffs[i])
+				}
+			}
+		})
+	}
+}
+
+func TestNaive_randomnessPolyCForResponseZetaC(t *testing.T) {
+	pp := DefaultPP
+	tests := []struct {
+		name      string
+		times     int
+		slotNum   int
+		precision float64
+		baseline  float64
+		manual    bool
+	}{
+		{
+			name:      "10000Time",
+			times:     10_000,
+			slotNum:   10,
+			precision: 1e-1,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "50000Time",
+			times:     50_000,
+			slotNum:   10,
+			precision: 1e-2,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "100000Time",
+			times:     100_000,
+			slotNum:   10,
+			precision: 1e-3,
+			baseline:  0.1,
+			manual:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			count := make([]int, tt.slotNum)
+			step := (pp.paramEtaC - int64(pp.paramBetaC) + 4) / 5
+			start := -(pp.paramEtaC - int64(pp.paramBetaC))
+			end := pp.paramEtaC - int64(pp.paramBetaC)
+
+			var polyC *PolyC
+			var err error
+			for cnt := 0; cnt < tt.times; cnt++ {
+				polyC, err = pp.randomPolyCForResponseZetaC()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				for i := 0; i < pp.paramDC; i++ {
+					switch {
+					case polyC.coeffs[i] < start:
+						t.Fatal("ERROR: Sample in left out")
+					case polyC.coeffs[i] > end:
+						t.Fatal("ERROR: Sample in right out")
+					default:
+						slot := (polyC.coeffs[i] - start) / step
+						count[slot] = count[slot] + 1
+					}
+				}
+			}
+			total := 0
+			for i := 0; i < tt.slotNum; i++ {
+				total += count[i]
+			}
+			for i := 0; i < tt.slotNum; i++ {
+				ratio := float64(count[i]) / float64(total)
+				if ratio-tt.baseline > tt.precision {
+					t.Errorf("slot %d, number %v, percent:%v", i, count[i], ratio)
+				}
+			}
+
+			if tt.manual {
+				for i := 0; i < pp.paramDA; i++ {
+					fmt.Println(polyC.coeffs[i])
+				}
+			}
+		})
+	}
+}
+
+func TestNaive_randomPolyCinEtaC(t *testing.T) {
+	pp := DefaultPP
+	tests := []struct {
+		name      string
+		times     int
+		slotNum   int
+		precision float64
+		baseline  float64
+		manual    bool
+	}{
+		{
+			name:      "10000Time",
+			times:     10_000,
+			slotNum:   10,
+			precision: 1e-1,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "50000Time",
+			times:     50_000,
+			slotNum:   10,
+			precision: 1e-2,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "100000Time",
+			times:     100_000,
+			slotNum:   10,
+			precision: 1e-3,
+			baseline:  0.1,
+			manual:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			count := make([]int, tt.slotNum)
+			step := (pp.paramEtaC + 4) / 5
+			start := -pp.paramEtaC
+			end := pp.paramEtaC
+
+			var polyC *PolyC
+			var err error
+			for cnt := 0; cnt < tt.times; cnt++ {
+				polyC, err = pp.randomPolyCinEtaC()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				for i := 0; i < pp.paramDC; i++ {
+					switch {
+					case polyC.coeffs[i] < start:
+						t.Fatal("ERROR: Sample in left out")
+					case polyC.coeffs[i] > end:
+						t.Fatal("ERROR: Sample in right out")
+					default:
+						slot := (polyC.coeffs[i] - start) / step
+						count[slot] = count[slot] + 1
+					}
+				}
+			}
+			total := 0
+			for i := 0; i < tt.slotNum; i++ {
+				total += count[i]
+			}
+			for i := 0; i < tt.slotNum; i++ {
+				ratio := float64(count[i]) / float64(total)
+				if ratio-tt.baseline > tt.precision {
+					t.Errorf("slot %d, number %v, percent:%v", i, count[i], ratio)
+				}
+			}
+
+			if tt.manual {
+				for i := 0; i < pp.paramDA; i++ {
+					fmt.Println(polyC.coeffs[i])
+				}
+			}
+		})
+	}
+}
+
+func TestNaive_randomPolyAinEtaA(t *testing.T) {
+	pp := DefaultPP
+	tests := []struct {
+		name      string
+		times     int
+		slotNum   int
+		precision float64
+		baseline  float64
+		manual    bool
+	}{
+		{
+			name:      "10000Time",
+			times:     10_000,
+			slotNum:   10,
+			precision: 1e-1,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "50000Time",
+			times:     50_000,
+			slotNum:   10,
+			precision: 1e-2,
+			baseline:  0.1,
+			manual:    false,
+		},
+		{
+			name:      "100000Time",
+			times:     100_000,
+			slotNum:   10,
+			precision: 1e-3,
+			baseline:  0.1,
+			manual:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			count := make([]int, tt.slotNum)
+			step := (pp.paramEtaA + 4) / 5
+			start := -pp.paramEtaA
+			end := pp.paramEtaA
+			var polyA *PolyA
+			var err error
+			for cnt := 0; cnt < tt.times; cnt++ {
+				polyA, err = pp.randomPolyAinEtaA()
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				for i := 0; i < pp.paramDA; i++ {
+					switch {
+					case polyA.coeffs[i] < start:
+						t.Fatal("ERROR: Sample in left out")
+					case polyA.coeffs[i] > end:
+						t.Fatal("ERROR: Sample in right out")
+					default:
+						slot := (polyA.coeffs[i] - start) / step
+						count[slot] = count[slot] + 1
+					}
+				}
+			}
+			total := 0
+			for i := 0; i < tt.slotNum; i++ {
+				total += count[i]
+			}
+			for i := 0; i < tt.slotNum; i++ {
+				ratio := float64(count[i]) / float64(total)
+				if ratio-tt.baseline > tt.precision {
+					t.Errorf("slot %d, number %v, percent:%v", i, count[i], ratio)
+				}
+			}
+
+			if tt.manual {
+				for i := 0; i < pp.paramDA; i++ {
+					fmt.Println(polyA.coeffs[i])
+				}
+			}
+		})
+	}
+}
+
+func TestNaive_randomPolyAinGammaA5(t *testing.T) {
+	pp := DefaultPP
+	tests := []struct {
+		name      string
+		times     int
+		slotNum   int
+		precision float64
+		baseline  float64
+		manual    bool
+	}{
+		{
+			name:      "10000Time",
+			times:     10_000,
+			slotNum:   11,
+			precision: 1e-1,
+			baseline:  float64(1) / float64(11),
+			manual:    false,
+		},
+		{
+			name:      "50000Time",
+			times:     50_000,
+			slotNum:   11,
+			precision: 1e-2,
+			baseline:  float64(1) / float64(11),
+			manual:    false,
+		},
+		{
+			name:      "100000Time",
+			times:     100_000,
+			slotNum:   11,
+			precision: 1e-3,
+			baseline:  float64(1) / float64(11),
+			manual:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			count := make([]int, tt.slotNum)
+			start := int64(-5)
+			end := int64(5)
+			var polyA *PolyA
+			var err error
+			for cnt := 0; cnt < tt.times; cnt++ {
+				polyA, err = pp.randomPolyAinGammaA5(RandomBytes(RandSeedBytesLen))
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				for i := 0; i < pp.paramDA; i++ {
+					switch {
+					case polyA.coeffs[i] < start:
+						t.Fatal("ERROR: Sample in left out")
+					case polyA.coeffs[i] > end:
+						t.Fatal("ERROR: Sample in right out")
+					default:
+						count[polyA.coeffs[i]-start] = count[polyA.coeffs[i]-start] + 1
+					}
+				}
+			}
+			total := 0
+			for i := 0; i < tt.slotNum; i++ {
+				total += count[i]
+			}
+			for i := 0; i < tt.slotNum; i++ {
+				ratio := float64(count[i]) / float64(total)
+				if ratio-tt.baseline > tt.precision {
+					t.Errorf("slot %d, number %v, percent:%v", i, count[i], ratio)
+				}
+			}
+
+			if tt.manual {
+				for i := 0; i < pp.paramDA; i++ {
+					fmt.Println(polyA.coeffs[i])
+				}
+			}
+		})
+	}
+}
